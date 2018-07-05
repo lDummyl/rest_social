caeee8faa21f3a0b8a33b6befabe91d68c62fef6
805268 <Ludovdim@gmail.com>
2018-07-05 15:03:32 +0300

    now js getting messages directly from server.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,7 @@
+
+
+var messageApi = Vue.resource('/message{/id}');
+
 Vue.component('message-row', {
     props: ['message'],
     template: '<div><i>({{message.id}})</i>{{ message.text }}</div>'
@@ -5,16 +9,23 @@ Vue.component('message-row', {
 
 Vue.component('messages-list', {
     props: ['messages'],
-    template: '<div><message-row v-for="message in messages" :message="message" /></div>'
+    template: '<div>  ' +
+
+    '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+
+    '</div>',
+    created: function () {
+        messageApi.get().then(result =>
+        result.json().then(data =>
+            data.forEach(message => this.messages.push(message))
+        )
+        )
+    }
 });
 var app = new Vue({
     el: '#app',
     template:'<messages-list :messages="messages" />',
     data: {
-        messages: [
-            {id:'1', text: 'Wow'},
-            {id:'2', text: 'Wo'},
-            {id:'3', text: 'W'},
-        ]
+        messages: []
     }
 })
